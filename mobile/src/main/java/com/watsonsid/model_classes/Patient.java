b9378146a38810f276e5a4ff48dee472094db027
Lance Cummins <lancehc@umich.edu>
2014-11-16 23:22:59 -0500

    merge I guess?

---
+++ mobile/src/main/java/com/watsonsid/model_classes/Patient.java
@@ -0,0 +1,14 @@
+package com.watsonsid.model_classes;
+
+/**
+ * Created by Daniel on 11/13/2014.
+ */
+public class Patient {
+    Patient(String _name, int _id)
+    {
+        name = _name;
+        id = _id;
+    }
+    String name;
+    int id;
+}
