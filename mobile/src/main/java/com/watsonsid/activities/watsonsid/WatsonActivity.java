a7f73f85c316eb697cf416f1759cf0f4e2dc77ab
ddthegreat <dfirsht@gmail.com>
2014-11-26 00:39:15 -0500

    worked on doctorhome page, it works now, mostly...

--- mobile/src/main/java/com/watsonsid/activities/watsonsid/WatsonActivity.java
+++ mobile/src/main/java/com/watsonsid/activities/watsonsid/WatsonActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.lance.watsonsid.R;
+import com.parse.ParseUser;
 import com.watsonsid.common.navdrawer.AbstractNavDrawerActivity;
 import com.watsonsid.fragments.WatsonFragment;
 
@@ -11,10 +12,18 @@ public class WatsonActivity extends AbstractNavDrawerActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, SampleDispatchActivity.class));
+        //startActivity(new Intent(this, SampleDispatchActivity.class));
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new WatsonFragment()).commit();
     }
 
     @Override
-    protected int getMainLayout() { return R.layout.activity_base; }
+    protected int getMainLayout() {
+        ParseUser user = ParseUser.getCurrentUser();
+        if(user.getBoolean("isPatient")) {
+            return R.layout.activity_base;
+        }
+        else {
+            return R.layout.activity_base_no_nav;
+        }
+    }
 }
---
+++ mobile/src/main/java/com/watsonsid/activities/watsonsid/WatsonActivityNoNav.java
@@ -0,0 +1,17 @@
+package com.watsonsid.activities.watsonsid;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.lance.watsonsid.R;
+import com.watsonsid.fragments.WatsonFragment;
+
+public class WatsonActivityNoNav extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_no_nav);
+        //startActivity(new Intent(this, SampleDispatchActivity.class));
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new WatsonFragment()).commit();
+    }
+}
