53839aaed216babb2091a96fdf56c42ab2215103
Lance Cummins <lancehc@umich.edu>
2014-11-25 23:33:22 -0800

    fixed dumb error from merge

--- mobile/src/main/java/com/watsonsid/fragments/DoctorHomeFragment.java
+++ mobile/src/main/java/com/watsonsid/fragments/DoctorHomeFragment.java
@@ -95,8 +95,8 @@ public class DoctorHomeFragment extends Fragment {
                     if(queriesFinished.intValue() == patientUsernames.size()){
                         setPatientStatus();
                     }
-                });
-            }
+                }}
+            );
         }
 
     }
